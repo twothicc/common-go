0068ec48710fd08aef7fdd4ad8cc20369fe1f37f
twothicc <chenenliuu@gmail.com>
2022-08-12 20:21:30 +0800

    test tag values

--- logger/init.go
+++ logger/init.go
@@ -73,6 +73,7 @@ func WithContext(ctx context.Context) *zap.Logger {
 	}
 
 	tags := grpc_ctxtags.Extract(ctx)
+	fmt.Println(tags.Values())
 
 	for _, tagKey := range defaultLogFields {
 		if tags.Has(tagKey) {
